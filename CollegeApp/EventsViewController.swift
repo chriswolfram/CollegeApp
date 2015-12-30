a5aad25383b5643a0b442017a8042899dfbd0ae3
Christopher Wolfram <christopher@wolfram.com>
2015-12-30 03:40:05 -0500

    Added basic event view

---
+++ CollegeApp/EventsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EventsView.swift
+//  CollegeApp
+//
+//  Created by Christopher Wolfram on 12/30/15.
+//  Copyright © 2015 Zalto Technologies. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UITableViewController
+{
+    
+}
