d73fff558df0ebeb0c84a4f3f2546526d94df764
Christopher Wolfram <christopher@wolfram.com>
2016-01-22 10:50:48 -0500

    Added detail view.

---
+++ CollegeApp/TourViewDetailController.swift
@@ -0,0 +1,14 @@
+//
+//  TourViewDetailController.swift
+//  CollegeApp
+//
+//  Created by Christopher Wolfram on 1/22/16.
+//  Copyright © 2016 Zalto Technologies. All rights reserved.
+//
+
+import UIKit
+
+class TourViewDetailController: UIViewController
+{
+    
+}
